61e19cfa9c62aa8e1c8cda65bdf81afa2974aefa
Emanuel Rodriguez <emrodle@gmail.com>
2020-05-25 22:47:37 -0600

    fectch

--- sw.js
+++ sw.js
@@ -43,20 +43,8 @@ self.addEventListener('install', e => {
     
   })
   
-  //cuando el navegador recupera una url
-  self.addEventListener('fetch', e => {
-    //Responder ya sea con el objeto en caché o continuar y buscar la url real
-    e.respondWith(
-      caches.match(e.request)
-        .then(res => {
-          if (res) {
-            //recuperar del cache
-            return res
-          }
-          //recuperar de la petición a la url
-          return fetch(e.request)
-        })
-        
-    )
-    
-  })
+  self.addEventListener('fetch', function(e){
+	e.respondWith(
+		caches.match(e.request).then(function(response){
+            return response || fetch(e.request);}
+            ))})
